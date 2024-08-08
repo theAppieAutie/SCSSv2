522e1366c1c48418f9c74910ef0c008d76de6e10
theappieautie <horgan-brown-p@proton.me>
2024-08-08 10:38:20 +0100

    amended vercel.json

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -1,6 +1,6 @@
 
 // configure changable data
-exports.config = {
+export const config = {
     trialLength: .1, // minutes for each trial
     advisorAccuracy : 90, // int representing percentage of the time advisor is correct
     packetTimeOnScreen : 10, // seconds it takes for each packet to navigate to the centre
